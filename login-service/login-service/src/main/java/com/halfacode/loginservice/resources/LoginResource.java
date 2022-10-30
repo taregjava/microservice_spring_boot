12a125bb94431a7b5e81fb44a9560edc81b7dbfb
tareg <tareg.safi501@gmail.com>
2022-10-31 00:17:22 +0400

    register microservice in api gateway

--- login-service/login-service/src/main/java/com/halfacode/loginservice/resources/LoginResource.java
+++ login-service/login-service/src/main/java/com/halfacode/loginservice/resources/LoginResource.java
@@ -14,10 +14,12 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Data
 @RestController
+//@RequestMapping("/auth")
 public class LoginResource {
 
 
