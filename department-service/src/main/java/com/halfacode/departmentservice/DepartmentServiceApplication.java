de133188c9c0ab4e28f5b499b623dd188c947921
tareg <tareg.safi501@gmail.com>
2022-11-05 01:23:23 +0400

    restemplate and fiegn client branch

--- department-service/src/main/java/com/halfacode/departmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/halfacode/departmentservice/DepartmentServiceApplication.java
@@ -10,7 +10,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 public class DepartmentServiceApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args ) {
+
 		SpringApplication.run(DepartmentServiceApplication.class, args);
 	}
 
